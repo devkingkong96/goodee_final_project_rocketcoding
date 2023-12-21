406b1902794bc87876209e8731deed7e8ad57b31
ksj <kimsj9693@gmail.com>
2023-12-21 20:51:40 +0900

    ksj_security 적용중

--- finalproject_rocket/src/main/java/com/rocket/ksj/member/model/dto/Member.java
+++ finalproject_rocket/src/main/java/com/rocket/ksj/member/model/dto/Member.java
@@ -1,6 +1,13 @@
 package com.rocket.ksj.member.model.dto;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Member {
+public class Member implements UserDetails{
 	private String userId;
 	private String password;
 	private String userName;
@@ -22,4 +29,38 @@ public class Member {
 	private String address;
 	private String hobby;
 	private Date enrollDate;
+	
+	//계정이 갖고 있는 권한 목록 리턴하는 메소드
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> auth=new ArrayList<>();
+		
+		auth.add(new SimpleGrantedAuthority("user"));
+		
+		if(userId.equals("admin")) auth.add(new SimpleGrantedAuthority("admin"));
+		
+		return auth;
+	}
+	@Override
+	public String getUsername() {
+		return this.userId;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	
+	
 }

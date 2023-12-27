f988c23d74c6a06b5595ac4069d39c41a2c4486e
JangSeYoung-BackEnd <umpire12@naver.com>
2023-12-27 09:20:59 +0900

    sy:dao 수정

--- finalproject_rocket/src/main/java/com/rocket/jsy/employee/model/dao/EmployeeDaolmpl.java
+++ finalproject_rocket/src/main/java/com/rocket/jsy/employee/model/dao/EmployeeDaolmpl.java
@@ -3,29 +3,31 @@ package com.rocket.jsy.employee.model.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 
 import com.rocket.jsy.employee.model.dto.Employee;
 
+@Repository
 public class EmployeeDaolmpl implements EmployeeDao{
 
 	@Override
 	public List<Employee> selectEmployeeAll(SqlSession session) {
-		return session.selectList("");
+		return session.selectList("employee.selectEmployeeAll");
 	}
 
 	@Override
 	public Employee selectEmployeeByNo(SqlSession session, String empNo) {
-		return session.selectOne("",empNo);
+		return session.selectOne("employee.selectEmployeeByNo",empNo);
 	}
 
 	@Override
 	public int insertEmployee(SqlSession session, Employee e) {
-		return session.insert("",e);
+		return session.insert("employee.insertEmployee",e);
 	}
 
 	@Override
 	public int updateEmployee(SqlSession session, Employee e) {
-		return session.update("",e);
+		return session.update("employee.updateEmployee",e);
 	}
 	
 }

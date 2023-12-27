79f7dad11df1be42088bb049db541f943f586284
JangSeYoung-BackEnd <umpire12@naver.com>
2023-12-27 20:32:39 +0900

    sy: dto수정

--- finalproject_rocket/src/main/java/com/rocket/jsy/employee/model/dto/Department.java
+++ finalproject_rocket/src/main/java/com/rocket/jsy/employee/model/dto/Department.java
@@ -1,5 +1,14 @@
 package com.rocket.jsy.employee.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 	private int depCode;
 	private String depName;

caf3001174291d6d4e8c664b27eb6c97e0d2ffd6
P-LEG\Administrator <seoj.dev@gmail.com>
2023-12-31 22:53:17 +0900

    urd 완료(반대지점 관리는 아직)

--- finalproject_rocket/src/main/java/com/rocket/FinalprojectRocketApplication.java
+++ finalproject_rocket/src/main/java/com/rocket/FinalprojectRocketApplication.java
@@ -2,12 +2,14 @@ package com.rocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class FinalprojectRocketApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinalprojectRocketApplication.class, args);
 	}
-
+	
 }
--- finalproject_rocket/src/main/java/com/rocket/seoj/logistics/model/service/LogisticsService.java
+++ finalproject_rocket/src/main/java/com/rocket/seoj/logistics/model/service/LogisticsService.java
@@ -1,13 +1,13 @@
 package com.rocket.seoj.logistics.model.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rocket.seoj.logistics.model.dao.LogisticsDao;
-import com.rocket.seoj.logistics.model.dto.Inventory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,8 +20,48 @@ public class LogisticsService {
 	
 	// 가안
 	@Transactional
-	public List<Inventory> selectAllInventories() {
+	public List<Map<String, Object>> selectAllInventories() {
 		return dao.selectAllInventories(session);
+	}
+	
+	@Transactional
+	public int updateColumn(long id,
+	                        String columnName,
+	                        String tableName,
+	                        String value,
+	                        String parentTableName,
+	                        String parentColumnId,
+	                        String parentColumnName,
+	                        String columnId) {
+		return dao.updateColumn(session, id, columnName, tableName, value,
+		                        parentTableName, parentColumnId, parentColumnName,
+		                        columnId);
+	}
+	
+	@Transactional
+	public boolean deleteInventoryAndAttachmentAndPrdIv(Long inventoryId) {
+		// TODO Auto-generated method stub
+		int attachDeleteResult = dao.deleteInventoryAttach(session, inventoryId);
+		System.out.println("attachDeleteResult " + attachDeleteResult);
+		int prdInventoryDelResult = dao.deletePrdInventory(session, inventoryId);
+		System.out.println("prdInventoryDelResult " + prdInventoryDelResult);
+		int inventoryDeleteResult
+		   = dao.deleteInventoryColumn(session, inventoryId);
+		System.out.println("inventoryDeleteResult " + inventoryDeleteResult);
+		
+		// 두 작업 중 하나라도 실패하면 롤백됩니다.
+		if (attachDeleteResult > 0 && inventoryDeleteResult > 0
+		   && prdInventoryDelResult > 0) {
+			System.out.println("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ인벤토리 딜리트 성공ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ");
+			return true;
+		} else if (inventoryDeleteResult > 0) {
+			System.out.println("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ인벤토리 딜리트 성공ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ");
+			return true;
+		} else {
+			// 예외를 던지거나 false를 반환하여 롤백됨을 나타낼 수 있습니다.
+			System.out.println("ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ인벤토리 딜리트 실패ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ");
+			return false;
+		}
 	}
 	
 }

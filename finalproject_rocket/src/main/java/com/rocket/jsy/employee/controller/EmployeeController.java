328dd5c581b0ae30c5fece8eeacfdf93732f43a1
JangSeYoung-BackEnd <umpire12@naver.com>
2024-01-04 14:44:21 +0900

    sy: 이제 안할게요

--- finalproject_rocket/src/main/java/com/rocket/jsy/employee/controller/EmployeeController.java
+++ finalproject_rocket/src/main/java/com/rocket/jsy/employee/controller/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.rocket.jsy.employee.controller;
 
-<<<<<<< HEAD
 
 import java.util.List;
 import java.util.Map;
@@ -74,60 +73,3 @@ public class EmployeeController {
 
 }
 
-=======
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.rocket.jsy.employee.model.dto.Employee;
-import com.rocket.jsy.employee.service.EmployeeService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
-@Controller
-@RequiredArgsConstructor
-public class EmployeeController {
-	
-	private final EmployeeService service;
-	
-	@GetMapping("/mypage")
-	public String mypage(Model model, Authentication authentication) {
-//	    if(authentication != null && authentication.getPrincipal() instanceof UserDetails) {
-	       // UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-	        Employee employee=(Employee)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//	    	String empNo = userDetails.getUsername();
-//	        log.info("{}", employee);
-//	        Employee employee = service.selectEmployeeByNo(empNo);
-	        model.addAttribute("employee", employee);
-//	    }
-	    return "employee/mypage";
-	}
-	
-	@GetMapping("/employeelist")
-	public String employeelist(Model model) {
-	    List<Employee> employees = service.selectEmployeeAll();
-	    model.addAttribute("employees", employees);
-	    return "employee/employeelist";
-	}
-	
-	@GetMapping("/employeeholidaylist")
-	public String employeeholidaylist(Model model) {
-		List<Employee> employees=service.selectEmployeeHolidayAll();
-		model.addAttribute("employees",employees);
-		return "employee/employeeholidaylist";
-	}
-	 @GetMapping("/myPageCalendar")
-	 @ResponseBody
-	    public List<Map<String, Object>> myPageCalendar() {
-	        return service.selectEmployeeMyPageCalendar();
-	    }
-}
->>>>>>> branch 'dev' of https://github.com/devkingkong96/final_project_rocketcoding.git
